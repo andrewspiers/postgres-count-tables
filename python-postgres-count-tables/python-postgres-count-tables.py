7731f97809f2189dd78ce599d4176e0e24b7d7bf
Andrew Spiers <andrew@andrewspiers.net>
2013-10-09 00:33:55 +1100

    Initial signs of success
    Added functions to make the db connection and actually count the tables.
    Seems to work.

--- python-postgres-count-tables/python-postgres-count-tables.py
+++ python-postgres-count-tables/python-postgres-count-tables.py
@@ -24,6 +24,7 @@ import ConfigParser
 import os
 import StringIO
 
+import psycopg2
 def findconfig():
     """return a ConfigParser object containing the configuration if one is
     found, else return None."""
@@ -41,7 +42,7 @@ def findconfig():
     #print (locations)
     #print ("configuration file found at " + " ".join(l))
     return l
-    
+
 def defaultconfig():
     """return a ConfigParser containing some defaults."""
     wconfig = ConfigParser.ConfigParser()
@@ -67,8 +68,38 @@ def stringconfig(config):
     flo.seek(0)
     return flo.read()
 
+def makeconnection(config):
+    """
+    given a config, return a connection.
+    """
+    conn = psycopg2.connect(database = config.get('Main','dbname'),
+                            user     = config.get('Main','username'),
+                            password = config.get('Main','password'),
+                            host     = config.get('Main','host'),
+                            )
+    return conn
+
+def counttables(conn):
+    """
+    given a connection to a database (conn), return the number of tables in
+    that database.
+
+    Excludes tables in the information_schema and pg_catalog schemas.
+    """
+    query =  "SELECT COUNT(table_name) FROM information_schema.tables "
+    query += "WHERE table_schema != 'pg_catalog' AND table_schema != "
+    query += "'information_schema';"
+    print (query)
+    cur = conn.cursor()
+    cur.execute(query)
+    tablecount=cur.fetchone()[0]
+    cur.close()
+    return tablecount
+
+
 
 findconfig()
 default = defaultconfig()
 printabledefault = stringconfig(default)
 print (printabledefault)
+print (counttables(makeconnection(defaultconfig())))
