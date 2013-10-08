34343760d95513f12adda7007cb488af4da67de5
Andrew Spiers <andrew@andrewspiers.net>
2013-10-09 00:47:25 +1100

    pep8

--- python-postgres-count-tables/python-postgres-count-tables.py
+++ python-postgres-count-tables/python-postgres-count-tables.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env/python2
+
+
 #python-postgres-count-tables.py
 
 """
@@ -25,23 +27,28 @@ import os
 import StringIO
 
 import psycopg2
+
+
 def findconfig():
     """return a ConfigParser object containing the configuration if one is
     found, else return None."""
     config = ConfigParser.ConfigParser()
     locations = []
     locations.append(os.getenv('PPCT_CONFIG'))
-    locations.append(os.path.join(os.getcwd(),'ppct_config'))
+    locations.append(os.path.join(os.getcwd(),  'ppct_config'))
     if os.getenv('XDG_CONFIG_HOME'):
-        locations.append(os.path.join(os.getenv('XDG_CONFIG_HOME'), 'ppct_config'))
+        locations.append(os.path.join(os.getenv('XDG_CONFIG_HOME'),
+                                      'ppct_config'))
     else:
-        locations.append(os.path.join(os.getenv('HOME'),'.config/ppct_config'))
+        locations.append(os.path.join(os.getenv('HOME'),
+                         '.config/ppct_config'))
     locations.append('/etc/ppct_config')
-    locations = [ item for item in locations if item ]  #strip out any None
+    locations = [item for item in locations if item]  # strip out any None
     l = config.read(locations)
     #print (locations)
     #print ("configuration file found at " + " ".join(l))
     return l
+
 
 def defaultconfig():
     """return a ConfigParser containing some defaults."""
@@ -49,35 +56,38 @@ def defaultconfig():
     wconfig.add_section('Main')
     #dbname,user,password,host
     #default dbname : username
-    wconfig.set('Main','dbname',os.getlogin())
+    wconfig.set('Main', 'dbname', os.getlogin())
     #default user   : username
-    wconfig.set('Main','username',os.getlogin())
+    wconfig.set('Main', 'username', os.getlogin())
     #default password : empty string
-    wconfig.set('Main','password','')
+    wconfig.set('Main', 'password', '')
     #default host     : localhost
-    wconfig.set('Main','host','localhost')
+    wconfig.set('Main', 'host', 'localhost')
     return wconfig
+
 
 def stringconfig(config):
     """
     return a given ConfigParser as a string
     """
-    flo = StringIO.StringIO()  #file like object
+    flo = StringIO.StringIO()  # file like object
     config.write(flo)
     flo.flush()
     flo.seek(0)
     return flo.read()
 
+
 def makeconnection(config):
     """
     given a config, return a connection.
     """
-    conn = psycopg2.connect(database = config.get('Main','dbname'),
-                            user     = config.get('Main','username'),
-                            password = config.get('Main','password'),
-                            host     = config.get('Main','host'),
+    conn = psycopg2.connect(database=config.get('Main', 'dbname'),
+                            user=config.get('Main', 'username'),
+                            password=config.get('Main', 'password'),
+                            host=config.get('Main', 'host'),
                             )
     return conn
+
 
 def counttables(conn):
     """
@@ -86,20 +96,19 @@ def counttables(conn):
 
     Excludes tables in the information_schema and pg_catalog schemas.
     """
-    query =  "SELECT COUNT(table_name) FROM information_schema.tables "
+    query = "SELECT COUNT(table_name) FROM information_schema.tables "
     query += "WHERE table_schema != 'pg_catalog' AND table_schema != "
     query += "'information_schema';"
-    print (query)
+    print(query)
     cur = conn.cursor()
     cur.execute(query)
-    tablecount=cur.fetchone()[0]
+    tablecount = cur.fetchone()[0]
     cur.close()
     return tablecount
-
 
 
 findconfig()
 default = defaultconfig()
 printabledefault = stringconfig(default)
-print (printabledefault)
-print (counttables(makeconnection(defaultconfig())))
+print(printabledefault)
+print(counttables(makeconnection(defaultconfig())))
