9cafbcb30fb9ca5a45c31e2288dff83e156dce0f
Andrew Spiers <andrew@andrewspiers.net>
2013-10-14 22:49:42 +1100

    prepare 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
         name = 'postgres_count_tables',
         url=
         'https://bitbucket.org/andrewspiers/python-postgres-count-tables',
-        version='0.1.0dev',
+        version='0.1.0',
         packages=['postgres_count_tables',],
         license='Apache 2.0',
         long_description=open('README.txt').read(),
